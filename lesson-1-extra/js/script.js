7a73586fe6fce898c5bc9c0146d1201bc1c236ec
KirillKondratyuk <kondks@yandex.ru>
2019-05-05 19:25:58 +0200

    дз7

--- lesson-1-extra/js/script.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-let num = 33721;
-
-console.log(3 * 3 * 7 * 2 * 1)
-
---
+++ lesson-7/clock/script.js
@@ -0,0 +1,5 @@
+window.setInterval(function(){
+	let now = new Date();
+	let clock = document.getElementById("clock");
+	clock.innerHTML = now.toLocaleTimeString();
+}, 1000);
